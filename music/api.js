a82bbe2723621f75d49dc5eb7cead1c18fbb60c4
Pretty9581 <49890352+Pretty9581@users.noreply.github.com>
2020-03-12 19:54:17 +0800

    Update api.js

--- music/api.js
+++ music/api.js
@@ -1,20 +1,20 @@
-﻿[
+[
     {
-        "id":"1",
+        "id":1,
         "title":"李行亮-愿得一人心",
-        "pic":"./images/愿得一人心.jpg",
-        "link":"./mp3/李行亮 - 愿得一人心 (DJ版).mp3"
+        "pic":"https://pretty9581.github.io/music/images/愿得一人心.jpg",
+        "link":"https://pretty9581.github.io/music/mp3/李行亮 - 愿得一人心 (DJ版).mp3"
     },
     {
-        "id":"2",
+        "id":2,
         "title":"段奥娟-陪我长大",
-        "pic":"./images/陪我长大.jpg",
-        "link":"./mp3/段奥娟 - 陪我长大.mp3"
+        "pic":"https://pretty9581.github.io/music/images/陪我长大.jpg",
+        "link":"https://pretty9581.github.io/music/mp3/段奥娟 - 陪我长大.mp3"
     },
     {
         "id":3,
         "title":"郭明惠-无名之辈",
-        "pic":"./images/无名之辈.jpg",
-        "link":"./mp3/郭明惠 - 无名之辈.mp3"
+        "pic":"https://pretty9581.github.io/music/images/无名之辈.jpg",
+        "link":"https://pretty9581.github.io/music/mp3/郭明惠 - 无名之辈.mp3"
     }
 ]
