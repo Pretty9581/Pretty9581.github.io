0c58503c08ae37186ef86862e5f3db45451df89a
Pretty9581 <49890352+Pretty9581@users.noreply.github.com>
2020-03-12 20:04:03 +0800

    Add files via upload

---
+++ rank.js
@@ -0,0 +1,20 @@
+﻿[
+    {
+        "id":1,
+        "title":"李行亮-愿得一人心",
+        "pic":"https://pretty9581.github.io/music/images/愿得一人心.jpg",
+        "link":"https://pretty9581.github.io/music/mp3/李行亮 - 愿得一人心 (DJ版).mp3"
+    },
+    {
+        "id":2,
+        "title":"段奥娟-陪我长大",
+        "pic":"https://pretty9581.github.io/music/images/陪我长大.jpg",
+        "link":"https://pretty9581.github.io/music/mp3/段奥娟 - 陪我长大.mp3"
+    },
+    {
+        "id":3,
+        "title":"郭明惠-无名之辈",
+        "pic":"https://pretty9581.github.io/music/images/无名之辈.jpg",
+        "link":"https://pretty9581.github.io/music/mp3/郭明惠 - 无名之辈.mp3"
+    }
+]
